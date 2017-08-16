820335bdfeacf7abe806e041f898a964e5dda553
Ashley Elder <ashleyelder@Ashleys-MacBook-Pro.local>
2017-08-15 22:58:20 -0700

    adds post resource

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,33 @@
+class PostsController < ApplicationController
+
+  def new
+  	@post = Post.new
+  end
+
+  def show
+  	@post = Post.find(params[:id])
+  end
+
+  def create
+  	@post= Post.new(post_params)
+
+  	@post.save
+    redirect_to @post
+  end
+
+	def destroy
+		@post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+	end
+
+	def index
+		@posts = Post.all
+	end
+
+  private
+  	def post_params
+  		params.require(:post).permit(:title, :text)
+  	end
+end
