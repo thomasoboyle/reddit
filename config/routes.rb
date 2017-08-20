9c88c77814f6b242bf1ce5c520f63dac4299a178
Ashley Elder <ashleyelder@Ashleys-MacBook-Pro.local>
2017-08-19 18:20:04 -0700

    adds signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   get 'users/new'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
   resources :posts do
     resources :comments
   end
 
   root 'posts#index'
+
+  resources :users
 end
