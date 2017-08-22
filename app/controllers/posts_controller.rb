5eab907adcacbda3bd4c425df62f5f54aad2d9b3
Ashley Elder <ashleyelder@Ashleys-MacBook-Pro.local>
2017-08-21 23:31:21 -0700

    adds username to post show and index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,8 @@ class PostsController < ApplicationController
   end
 
   def create
-  	@post= Post.new(post_params)
+  	@post = Post.new(post_params)
+    @post.user = current_user
 
   	if @post.save
       redirect_to @post
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
+  belongs_to :user
   has_many :comments
 	validates :title, presence: true,
-										length: { minimum: 5}
+										length: { minimum: 5 }
 end
