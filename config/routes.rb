5ffc0a3da5120725559dbb407706f7a42cfced03
Ashley Elder <ashleyelder@Ashleys-MacBook-Pro.local>
2017-08-26 14:02:26 -0700

    update routes to use session

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'users/new'
   get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
-  get '/login', to: 'users#new'
-  post '/login', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
   resources :posts do
