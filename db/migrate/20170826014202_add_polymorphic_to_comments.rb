a06372f910dd2d98c21623a989e4e12c2f282428
Ashley Elder <ashleyelder@Ashleys-MacBook-Pro.local>
2017-08-27 13:15:43 -0700

    moves header to partial and updates lins to signup and login

--- db/migrate/20170826014202_add_polymorphic_to_comments.rb
+++
@@ -1,6 +0,0 @@
-class AddPolymorphicToComments < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :comments, :commentable, polymorphic: true
-    add_index :comments, [:commentable_id, :commentable_type]
-  end
-end
