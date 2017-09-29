3c3fb1ff87ec300e4e24586bd4fe8508855cefad
Ashley Elder <ashley.elder@procore.com>
2017-09-28 20:30:32 -0700

    schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170927065646) do
+ActiveRecord::Schema.define(version: 20170928035323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 20170927065646) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "upvotes"
     t.bigint "post_id"
     t.index ["parent_type", "parent_id"], name: "index_comments_on_parent_type_and_parent_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
@@ -35,6 +34,8 @@ ActiveRecord::Schema.define(version: 20170927065646) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "url"
+    t.string "subreddit"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
